b0358393f998b2d098f9a8e3eea52da5f10439fb
Bruno Jerković <bruno.jerkovic5@gmail.com>
2021-04-19 17:05:52 +0200

    API key hidden

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 dataset_location = 'Datasets/current_tweets.csv'
 refactored_dataset_location = r'C:\Users\sluzb\Documents\Fakultet\Radboud\Semester 1\Text And Multimedia Mining\Project\Code\IEEE_Dataset\refactored_datasets'
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOLLJQEAAAAAlQxzA7if4YQuFj4Wgo%2BmF3gVSf0%3DvhQxijexgI98GKGW7ch0BByywKWYQVs3n1LO55rBplpAY1LjXD'
-google_maps_api_key = 'AIzaSyD2v23S9-0bZqU8OxtvTGt26Yqx8n72XHk'
-google_geocode_api_key = 'AIzaSyClF7gGVQrVfWOPwULnyUOwuI4v8vicGm0'
+google_maps_api_key = '***' # NEEDS TO BE FILLED
+google_geocode_api_key = '***' # NEEDS TO BE FILLED
 
 ieee_username = 'sluzbeni1@gmail.com'
 ieee_password = '***' # NEEDS TO BE FILLED TO USE SELENIUM SCRAPING ON IEEE WEBSITE
